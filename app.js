7ceba4dc5e30bd6aebe6d8040dd04f7f66036a52
Patrick Hrabos <phrabos@gmail.com>
2020-12-16 19:34:57 -0500

    local storage now recalss form inputs on page refresh instead of button click

--- app.js
+++ app.js
@@ -1,47 +1,44 @@
-//alert("test"
-
-
 
 //input variables
 const nameInput = document.getElementById('name-input');
 const emailInput = document.getElementById('email-input');
 const messageInput = document.getElementById('message');
-//const test= storageOject.name;
 
 //button variables
 const sendButton = document.getElementById('send-button');
 const resetButton = document.getElementById('reset-button');
 const clearStorage = document.getElementById('clear-storage');
-const recallStorage = document.getElementById('recall-storage');
 
-//add eventListener
+// object for local storage
 const storageObject={};
 
+//add eventListener for form submit
 sendButton.addEventListener('click', ()=>{
     storageObject.name=nameInput.value;
     storageObject.email=emailInput.value;
     storageObject.message=messageInput.value;
 
-    localStorage.setItem('name',JSON.stringify(storageObject.name));
-    localStorage.setItem('email',JSON.stringify(storageObject.email));
-    localStorage.setItem('message',JSON.stringify(storageObject.message));
-
-
+    // set inputs into local storage
+    localStorage.setItem('name',(storageObject.name));
+    localStorage.setItem('email',(storageObject.email));
+    localStorage.setItem('message',(storageObject.message));
 });
 
-    
+//add eventListener to reset form inputs   
 resetButton.addEventListener('click', ()=>{
     nameInput.value="";
     emailInput.value="";
     messageInput.value="";    
 });
+
+//add eventListener to clear local storage
 clearStorage.addEventListener('click', ()=>{
     localStorage.clear();
-
 });
-recallStorage.addEventListener('load', ()=>{
-    alert('test')
-    nameInput.value = JSON.parse(localStorage.name);
-    emailInput.value = JSON.parse(localStorage.email);
-    messageInput.value = JSON.parse(localStorage.email);
+
+//add eventListener to recall local storage when page refreshes
+window.addEventListener('load', ()=>{
+    nameInput.value = localStorage.getItem('name');
+    emailInput.value = localStorage.getItem('email');
+    messageInput.value = localStorage.getItem('message');
 });
